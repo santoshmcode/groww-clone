9965af970db7dd19bc663851cc592ef85fe9bc6c
Dhirendra Kumar <dhirendra9032@gmail.com>
2021-07-24 17:25:28 +0530

    Forword function added for slide next

---
+++ Dhirendra/stocks.js
@@ -0,0 +1,219 @@
+
+
+let topmarket1 = {
+    "name": "Bajaj Finance",
+    "img": "/assets/mm1.png",
+    "price": "₹6180.90",
+    "percent": "-14.65(0.24%)"
+};
+let topmarket2 = {
+    "name": "Bharti Airtel",
+    "img": "/assets/mm2.png",
+    "price": "₹548.30",
+    "percent": "1.60(1.23%)"
+};
+let topmarket3 = {
+    "name": "Maruti Suzuki",
+    "img": "/assets/mm3.png",
+    "price": "₹7293.39",
+    "percent": "-5.70(1.35%)"
+};
+    
+let topmarket4= {
+    "name": "Axix Bank",
+    "img": "/assets/mm4.png",
+    "price": "₹755.50",
+    "percent": "8.65(1.16%)"
+};
+let topmarket5 = {
+    "name": "L&T",
+    "img": "/assets/mm5.png",
+    "price": "₹1161.75",
+    "percent": "-30.40(1.83%)"
+};
+    
+let topmarket6 = {
+    "name": "ONCG",
+    "img": "/assets/mm6.png",
+    "price": "₹115.30",
+    "percent": "-0.60(0.10%)"
+};
+let topmarket7 = {
+    "name": "Acian Paints",
+    "img": "/assets/mm7.png",
+    "price": "₹3083.75",
+    "percent": "-23.00(0.73%)"
+};
+let topmarket8 = {
+    "name": "HCL Technologies",
+    "img": "/assets/mm8.png",
+    "price": "₹994.00",
+    "percent": "15.60(2.23%)"
+};
+let topmarket9 = {
+    "name": "Bajaj Finacerv",
+    "img": "/assets/mm9.png",
+    "price": "₹13,201.30",
+    "percent": "136.60(5.23%)"
+};
+let topmarket10 = {
+    "name": "Wipro",
+    "img": "/assets/mm10.png",
+    "price": "₹599.30",
+    "percent": "14.60(1.53%)"
+};
+
+
+function forword() {
+
+    let one = document.getElementById('one');
+    one.style.color = "#00d090";
+    one.style.textDecoration = "bold"
+    one.style.fontSize = "20px"
+    one.textDecoration="u"
+   
+    let col_name1 = document.getElementById("col_name1");
+    col_name1.innerHTML = topmarket1.name;
+    
+    let col_img1 = document.getElementById("col_img1");
+    col_img1.src = topmarket1.img;
+
+    let col_price1 = document.getElementById("col_price1");
+    col_price1.innerHTML = topmarket1.price;
+
+    let col_per1 = document.getElementById("col_per1");
+    col_per1.innerHTML = topmarket1.percent;
+    
+
+
+     let col_name2 = document.getElementById("col_name2");
+    col_name2.innerHTML = topmarket2.name;
+    
+    let col_img2 = document.getElementById("col_img2");
+    col_img2.src = topmarket2.img;
+
+    let col_price2 = document.getElementById("col_price2");
+    col_price2.innerHTML = topmarket2.price;
+
+    let col_per2 = document.getElementById("col_per2");
+    col_per2.style.color = "#00d090";
+    col_per2.innerHTML = topmarket2.percent;
+    
+
+
+    let col_name3 = document.getElementById("col_name3");
+    col_name3.innerHTML = topmarket3.name;
+    
+    let col_img3 = document.getElementById("col_img3");
+    col_img3.src = topmarket3.img;
+
+    let col_price3 = document.getElementById("col_price3");
+    col_price3.innerHTML = topmarket3.price;
+
+    let col_per3 = document.getElementById("col_per3");
+    col_per3.innerHTML = topmarket3.percent;
+
+
+
+
+    let col_name4 = document.getElementById("col_name4");
+    
+    col_name4.innerHTML = topmarket4.name;
+    
+    let col_img4 = document.getElementById("col_img4");
+    col_img4.src = topmarket4.img;
+
+    let col_price4 = document.getElementById("col_price4");
+    col_price4.innerHTML = topmarket4.price;
+
+    let col_per4 = document.getElementById("col_per4");
+    col_per4.style.color = "#00d090";
+    col_per4.innerHTML = topmarket4.percent;
+
+
+
+    let col_name5 = document.getElementById("col_name5");
+    col_name5.innerHTML = topmarket5.name;
+    
+    let col_img5 = document.getElementById("col_img5");
+    col_img5.src = topmarket5.img;
+
+    let col_price5 = document.getElementById("col_price5");
+    col_price5.innerHTML = topmarket5.price;
+
+    let col_per5 = document.getElementById("col_per5");
+    col_per5.innerHTML = topmarket5.percent;
+
+
+
+    let col_name6 = document.getElementById("col_name6");
+    col_name6.innerHTML = topmarket6.name;
+    
+    let col_img6 = document.getElementById("col_img6");
+    col_img6.src = topmarket6.img;
+
+    let col_price6 = document.getElementById("col_price6");
+    col_price6.innerHTML = topmarket6.price;
+
+    let col_per6 = document.getElementById("col_per6");
+    col_per6.style.color = "#eb5b3c";
+    col_per6.innerHTML = topmarket6.percent;
+
+
+
+    let col_name7 = document.getElementById("col_name7");
+    col_name7.innerHTML = topmarket7.name;
+    
+    let col_img7 = document.getElementById("col_img7");
+    col_img7.src = topmarket7.img;
+
+    let col_price7 = document.getElementById("col_price7");
+    col_price7.innerHTML = topmarket7.price;
+
+    let col_per7 = document.getElementById("col_per7");
+    col_per7.style.color = "#eb5b3c";
+    col_per7.innerHTML = topmarket7.percent;
+
+
+    let col_name8 = document.getElementById("col_name8");
+    col_name8.innerHTML = topmarket8.name;
+    
+    let col_img8 = document.getElementById("col_img8");
+    col_img8.src = topmarket8.img;
+
+    let col_price8 = document.getElementById("col_price8");
+    col_price8.innerHTML = topmarket8.price;
+
+    let col_per8 = document.getElementById("col_per8");
+    col_per8.innerHTML = topmarket8.percent;
+
+
+
+    let col_name9 = document.getElementById("col_name9");
+    col_name9.innerHTML = topmarket9.name;
+    
+    let col_img9 = document.getElementById("col_img9");
+    col_img9.src = topmarket9.img;
+
+    let col_price9 = document.getElementById("col_price9");
+    col_price9.innerHTML = topmarket9.price;
+
+    let col_per9 = document.getElementById("col_per9");
+    col_per9.innerHTML = topmarket9.percent;
+
+    let col_name10 = document.getElementById("col_name10");
+    col_name10.innerHTML = topmarket10.name;
+    
+    let col_img10 = document.getElementById("col_img10");
+    col_img10.src = topmarket10.img;
+
+    let col_price10 = document.getElementById("col_price10");
+    col_price10.innerHTML = topmarket10.price;
+
+    let col_per10 = document.getElementById("col_per10");
+    col_per10.innerHTML = topmarket10.percent;
+
+    
+    }
+
+   
