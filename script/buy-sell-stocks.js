ef7e549135261861f37afeabd763abaffabbaf16
Santosh <santoshmane619@gmail.com>
2021-07-25 05:46:50 +0530

    Santosh: Added Buying stock fuctionality

---
+++ script/buy-sell-stocks.js
@@ -0,0 +1,36 @@
+let walletBalence;
+if (localStorage.getItem("walletBalence") == null) {
+    walletBalence = 0;
+} else {
+    walletBalence = JSON.parse(localStorage.getItem("walletBalence")) / 1;
+}
+
+let stockPrice = document.getElementById("buy-card-price-value");
+let stockNumber = document.getElementById("buy-card-stock-numbers-value");
+let companyOneStackPrice = document.getElementById("stock-company-price-value");
+
+
+let walletBalenceError = document.getElementById("wallet-blance-error");
+
+
+function updateStockPriceValue(e) {
+    if (e.key == "Enter") {
+        stockPrice.innerHTML = (companyOneStackPrice.innerHTML/1)* (stockNumber.value/1)
+        
+    }
+    
+}
+
+function buyStock() {
+    if (stockPrice.innerHTML / 1 > walletBalence / 1) {
+        walletBalenceError.innerHTML = "In sufficient wallet balance";
+    } else if (stockPrice.innerHTML / 1 <= walletBalence / 1) {
+        walletBalence = (walletBalence / 1) - (stockPrice.innerHTML / 1)
+        localStorage.setItem("walletBalence", walletBalence)
+        
+    }
+}
+
+function showWalletPage() {
+    window.location.href = "/pages/wallet.html";
+}
---
+++ script/wallet.js
@@ -0,0 +1,28 @@
+let walletBalence = 0
+if (localStorage.getItem("walletBalence") == null) {
+    localStorage.setItem("walletBalence",0)
+} else {
+    walletBalence = JSON.parse(localStorage.getItem("walletBalence"))
+}
+let currentWallet = document.getElementById("wallet-total-amount");
+let closingBalance = document.getElementById("closing-balance");
+let ongoingBalance = document.getElementById("ongoing-balance");
+let balance = document.getElementById("balance");
+let walletBalanceInput = document.getElementById("amount");
+
+currentWallet.innerHTML = walletBalence;
+closingBalance.innerHTML = walletBalence;
+balance.innerHTML = walletBalence;
+ongoingBalance.innerHTML = walletBalence;
+function updateWalletBalence() {
+    localStorage.setItem(
+        "walletBalence",
+        JSON.stringify(walletBalence/1 + walletBalanceInput.value/1)
+    );
+    currentWallet.innerHTML = JSON.parse(localStorage.getItem("walletBalence"));
+    closingBalance.innerHTML = JSON.parse(localStorage.getItem("walletBalence"));
+    balance.innerHTML = JSON.parse(localStorage.getItem("walletBalence"));
+    ongoingBalance.innerHTML = JSON.parse(
+        localStorage.getItem("walletBalence")
+    );;
+}
