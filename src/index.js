7c718569001304e335832c97003e63ec47a942b9
Santosh Mane <santoshmane619@gmail.com>
2021-08-25 14:39:59 +0530

    Added 404 page for invalid routes

--- src/index.js
+++ src/index.js
@@ -11,37 +11,32 @@ app.use(express.json());
 // This is alrady defined middleware for static files.
 app.use(express.static(path.join(__dirname, "../static")));
 
-
-
-// Register View engine 
-app.set('view engine', 'ejs')
-// setting path for views folder, 
+// Register View engine
+app.set("view engine", "ejs");
+// setting path for views folder,
 // requried only when your views folder having different name
-app.set('views', 'dynamic/views')
+app.set("views", "dynamic/views");
 
 // home page Route
-const homeRoute = require('./routes/home.route')
-app.use('/', homeRoute);
+const homeRoute = require("./routes/home.route");
+app.use("/", homeRoute);
 
 // dashboard page Route
-const dashboardRoute = require('./routes/dashboard.route')
-app.use('/dashboard', dashboardRoute);
+const dashboardRoute = require("./routes/dashboard.route");
+app.use("/dashboard", dashboardRoute);
 
 // 404 page
 app.use((req, res) => {
     try {
-        res.status(404).render('404')
-    } catch (err) {
-        
-    }
-})
-
+        res.status(404).render("404");
+    } catch (err) {}
+});
 
 app.listen(process.env.PORT, async () => {
-    try {        
-        await connect()
+    try {
+        await connect();
     } catch (error) {
-        console.log(error.message);        
+        console.log(error.message);
     }
     console.log(`Listning, http://localhost:${process.env.PORT}`);
 });
