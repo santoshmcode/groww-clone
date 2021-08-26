add166e31684319bc4cd57b733728a7d7153eb1a
Santosh Mane <santoshmane619@gmail.com>
2021-08-26 17:20:42 +0530

    Added sample buy-stock page

--- src/routes/dashboard.route.js
+++ src/routes/dashboard.route.js
@@ -1,6 +1,5 @@
-const express = require("express")
-const router = express.Router()
-
+const express = require("express");
+const router = express.Router();
 
 // Request comming form /dashboard
 router.get("/", (req, res) => {
@@ -21,9 +20,9 @@ router.get("/stocks", (req, res) => {
 
 router.get("/mutual-funds", (req, res) => {
     try {
-        res.status(200).render("dashboard/mutual-funds.ejs")
+        res.status(200).render("dashboard/mutual-funds.ejs");
     } catch (err) {
-        return res.status(400).send(err.message)
+        return res.status(400).send(err.message);
     }
 });
 
@@ -52,7 +51,21 @@ router.get("/us-stocks", (req, res) => {
 });
 
 
+// Changes using DB
 
-
+router.get("/buy-stocks", (req, res) => {
+    try {
+        let company = {
+            company_name: "HDFC Bank",
+            current_stock_price: 2345,
+            logo: "https://assets-netstorage.groww.in/stock-assets/logos/INE002A01018.png",
+            is_growing: true,
+            grow_value: 0.34,
+        };
+        res.status(200).render("buy-stocks.ejs", {company});
+    } catch (err) {
+        return res.status(400).send(err.message);
+    }
+});
 
 module.exports = router;
