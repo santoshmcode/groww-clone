e16c5525f2dc0f7379e958ac705f906342e71243
Santosh Mane <santoshmane619@gmail.com>
2021-08-28 02:29:22 +0530

    Added profile section 2

--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -5,10 +5,24 @@ const CurrentUser = require("../models/currentUser.model");
 const User = require("../models/users.model");
 
 router.get("/basic-details", async (req, res) => {
-    const currentUser = await CurrentUser.find().lean().exec();
-    const user = await User.findById(currentUser[0].userID);
-    console.log(user);
-    return res.status(200).render("profile/basic-details", {user})
+    try {
+        const currentUser = await CurrentUser.find().lean().exec();
+        const user = await User.findById(currentUser[0].userID);
+        return res.status(200).render("profile/basic-details", {user})
+    } catch (err) {
+        return res.status(400).send(err.message);
+    }
 });
+
+router.get("/holding-stocks", async (req, res) => {
+    try {
+        const currentUser = await CurrentUser.find().lean().exec();
+        const user = await User.findById(currentUser[0].userID);
+        return res.status(200).render("profile/holding-stocks", { user });
+    } catch (err) {
+        return res.status(400).send(err.message);
+    }
+})
+
 
 module.exports = router;
---
+++ static/scripts/profile.js
@@ -0,0 +1,7 @@
+function basic_profile() {
+    window.location.href = "/profile/basic-details";
+}
+
+function holding_stocks() {
+    window.location.href = "/profile/holding-stocks";
+}
