87979f94a960b188e7c34ee815f78cf423a4f6d5
Santosh Mane <santoshmane619@gmail.com>
2021-08-28 16:08:13 +0530

    Added Wellet Update Functionality

--- src/routes/wallet.route.js
+++ src/routes/wallet.route.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const bodyParser = require("body-parser");
 
-router.get('/', (req, res) => {
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+const CurrentUser = require("../models/currentUser.model");
+const User = require("../models/users.model");
+
+router.get("/", async (req, res) => {
     try {
-        return res.status(200).render("wallet.ejs")
+        const currentUser = await CurrentUser.findOne().lean().exec();
+        const userID = currentUser.userID;
+        const user = await User.findById(userID).lean().exec();
+        return res.status(200).render("wallet.ejs", { user });
     } catch (ree) {
-        return res.status(400).send(err.message)
+        return res.status(400).send(err.message);
     }
-})
+});
 
+router.post("/update_wallet", urlencodedParser, async (req, res) => {
+    try {
+        const data = req.body;
+
+        const currentUser = await CurrentUser.findOne().lean().exec();
+        const userID = currentUser.userID;
+        const user = await User.findById(userID).lean().exec();
+        let currentBalance = user.current_balance;
+        let toAdd = data.add_balance;
+        let total = currentBalance + toAdd/1;
+        await User.findByIdAndUpdate(userID, { current_balance : total})
+            .lean()
+            .exec();
+        return res.redirect("/wallet")
+    } catch (err) {
+        return res.status(400).send(err.message);
+    }
+});
 
 module.exports = router;
